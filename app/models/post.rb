a0243551f85a0d11a6237ec88232d9ff10944938
Zacharia <ndunguzachary24@gmail.com>
2022-10-03 14:31:43 +0300

    commit tree 97cb1d27bd21dd62dd8a76f9adc16ae82b477fc5

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title,   presence:  true
+    validates :content, length:  {minimum: 250 }
+    validates   :summary,  length:  {maximum:  250}
+    validates  :category,  inclusion: { in:  %w(Fiction  Non-Fiction)}
+    validates  :is_clickbait?
+
+custom_validations = ["Won't Beleve", "Secret", "Top[number]",  "Guess"]
+
+def is_clickbait? 
+if custom_validations.none? { |pat| pat.match title}
+eerors.add(:title, "must be clickbait")
+end 
 end
+
